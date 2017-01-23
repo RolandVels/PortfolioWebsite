ecdc42398205ed47cbfebafc123163b6bb050b6e
Rolands Vels-Zviedris <rolandsv-z@inbox.lv>
2017-01-23 18:44:41 +0200

    .

--- js/common.js
+++ js/common.js
@@ -7,24 +7,6 @@ $(function() {
 		});
 	};
 
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
 	//Chrome Smooth Scroll
 	try {
 		$.browserSelector();
@@ -62,7 +44,6 @@ $(function() {
 	});
 
 	function fadeIn(){
-		// $('.main-title, .more-work').hide();
 		$('.main-title, .more-work').delay(100).fadeIn(1000);
 	}
 	$(document).ready(fadeIn);
